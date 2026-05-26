ebc223a9cf880f90d536b6ff5551b0cc8fef6033
Lauriene <laurienea840@gmail.com>
2026-05-26 16:59:32 +0000

    andamento

---
+++ src/services/supabase.js
@@ -0,0 +1,9 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = 'SUA_URL'
+const supabaseKey = 'SUA_KEY'
+
+export const supabase = createClient(
+  supabaseUrl,
+  supabaseKey
+)
